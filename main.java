77ef6ec1814a17c28278d5a1c99040ef02c9cdb2
jagan03 <70654238+jagan03@users.noreply.github.com>
2020-10-12 18:07:17 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,21 @@
+package com.Employee;
+
+public class Main {
+
+    public Static void main(String[] args) {
+
+        Employee e1 = new Employee();
+        e1.name = "Saurabh";
+        e1.age = 23;
+        e1.city = "Chennai";
+        e1.display();
+
+        Employee e2 = new Employee();
+        e2.name = "Jagan";
+        e2.age = 20;
+        e2.city = "Chennai";
+        e2.display();
+
+
+    }
+}
